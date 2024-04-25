1c1425f22b100221967ebb95ebce017e9cf00fb8
ProjectWithRavi <raviyt73@gmail.com>
2024-04-25 11:59:48 +0530

    vector in c++

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,9 +1,48 @@
-// #include <bits/stdc++.h>
-// #include <vector>
-// using namespace std;
+//vector -->  dynamic array data structure
 
-// int main(){
-//     vector <int> element
-    
-//     return 0;
-// }
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+int main(){
+    // ******************************** add integer in vector
+
+    // vector <int> element;
+    // element.push_back(7);
+    // element.push_back(1);
+    // element.push_back(6);
+    // element.push_back(9);
+    // // cout<<element.size();
+    // for(int i=0; i<element.size(); i++){
+    //     cout<<element[i]<<endl;
+    // }
+
+
+    // ****************************** add string in vector 
+    // vector <string> mystr;
+    // mystr.push_back("ravi");
+    // mystr.push_back("ram");
+    // mystr.push_back("shyam");
+    // mystr.push_back("shiv");
+    // for(int i=0; i<mystr.size(); i++){
+    //     cout<<mystr[i]<<endl;
+    // }
+
+    // ************************ add value and print value in vector
+    vector<int> myvalue;
+    int vsize,value;
+    cout<<"enter vector size :";
+    cin>>vsize;
+    for(int i=0; i<vsize ;i++){
+        cout<<"enter vector value :";
+        cin>>value;
+        myvalue.push_back(value);
+    }
+
+    for(int i=0; i<vsize; i++){
+        cout<<myvalue[i]<<" ";
+    }
+
+
+    return 0;
+}
