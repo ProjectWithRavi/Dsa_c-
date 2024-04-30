f823abd4cb92ce7ed7481da9eb20791e8c1b79ec
ProjectWithRavi <raviyt73@gmail.com>
2024-04-30 19:46:51 +0530

    vector in c++

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -29,20 +29,71 @@ int main(){
     // }
 
     // ************************ add value and print value in vector
-    vector<int> myvalue;
-    int vsize,value;
-    cout<<"enter vector size :";
-    cin>>vsize;
-    for(int i=0; i<vsize ;i++){
-        cout<<"enter vector value :";
-        cin>>value;
-        myvalue.push_back(value);
+    // vector<int> myvalue;
+    // int vsize,value;
+    // cout<<"enter vector size :";
+    // cin>>vsize;
+    // for(int i=0; i<vsize ;i++){
+    //     cout<<"enter vector value :";
+    //     cin>>value;
+    //     myvalue.push_back(value);
+    // }
+
+    // for(int i=0; i<vsize; i++){
+    //     cout<<myvalue[i]<<" ";
+    // }
+
+
+    // return 0;
+
+
+
+    // *******************************8 xor oprator
+    // vector <int> myint;
+    // int vsize,value;
+    // cout<<"enter vector size :";
+    // cin>>vsize;
+
+    // for(int i=0; i<vsize; i++){
+    //     cout<<"enter vector item :";
+    //     cin>>value;
+    //     myint.push_back(value);
+    // }
+
+
+// ********************************** find Uniqe value
+  vector <int> narr;
+  vector <int> arr;
+  int vsize,vvalue;
+  cout<<"enter vector size ";
+  cin>>vsize;
+
+  // add item in vector
+  for(int i=0; i<vsize; i++){
+    cout<<"enter vector item ";
+    cin>>vvalue;
+    arr.push_back(vvalue);
+  }
+
+  // compare unique value is present or not
+  int value = 0;
+  for (int i = 0; i < arr.size(); i++) {
+    for (int j = 0; j < arr.size(); j++) {
+      if (0 == (arr[i] ^ arr[j])) {
+        value++;
+      }
     }
-
-    for(int i=0; i<vsize; i++){
-        cout<<myvalue[i]<<" ";
+    if (value == 1) {
+      int mvalue = arr[i];
+      narr.push_back(mvalue);
     }
+    value = 0;
+  }
 
-
-    return 0;
+ //print unique value
+  for (int i = 0; i < narr.size(); i++) {
+    cout << narr[i] << " value unique " << endl;
+  }
 }
+
+
